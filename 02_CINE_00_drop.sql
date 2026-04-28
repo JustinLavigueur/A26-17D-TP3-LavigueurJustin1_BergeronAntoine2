3365fc5e92e4c2e0a61a8b3ad1e5680c44d4bb60
Justin_Lavigueur <lavigueurjustin@gmail.com>
2026-04-28 11:28:19 -0400

    FIX : Correction du TP2 sur le fichiers objects.sql et petite modification de drop.sql

--- 02_CINE_00_drop.sql
+++ 02_CINE_00_drop.sql
@@ -3,6 +3,9 @@
 -- Ce fichier sql contient les commandes pour la suppression des tables de la base de données CINE en CASCADE (enlève les liens des données dans d'autres tables)
 --------------------------------------------------------
 
+DROP SEQUENCE cine.seq_clients;
+DROP VIEW cine.dictionnaire_v;
+
 DROP TABLE cine.cinemas CASCADE CONSTRAINTS;
 DROP TABLE cine.genres CASCADE CONSTRAINTS;
 DROP TABLE cine.films CASCADE CONSTRAINTS;
@@ -11,5 +14,3 @@ DROP TABLE cine.seances CASCADE CONSTRAINTS;
 DROP TABLE cine.clients CASCADE CONSTRAINTS;
 DROP TABLE cine.reservations CASCADE CONSTRAINTS;
 
-DROP SEQUENCE cine.seq_clients;
-DROP VIEW cine.dictionnaire_v;
