b44a7c0bb075eec3cbc2047ee70d23b36696e7da
Justin_Lavigueur <lavigueurjustin@gmail.com>
2026-05-11 13:31:56 -0400

    FIX : Modification dans securite.sql

--- 02_CINE_30_securite.sql
+++ 02_CINE_30_securite.sql
@@ -19,26 +19,21 @@ BEGIN
 -- 9) role_caissier
 -- ============================================================================
     FOR i IN 1 .. v_tables_caissier.COUNT LOOP
-            EXECUTE IMMEDIATE
-                'GRANT SELECT, INSERT, UPDATE, DELETE ON cine.' || v_tables_caissier(i) || ' TO role_caissier';
+            EXECUTE IMMEDIATE 'GRANT SELECT, INSERT, UPDATE, DELETE ON cine.' || v_tables_caissier(i) || ' TO role_caissier';
         END LOOP;
 
-        EXECUTE IMMEDIATE
-            'GRANT EXECUTE ON cine.gestion_cinema_pkg TO role_caissier';
+        EXECUTE IMMEDIATE 'GRANT EXECUTE ON cine.gestion_cinema_pkg TO role_caissier';
 
-        EXECUTE IMMEDIATE
-            'GRANT role_caissier TO caissier01';
+        EXECUTE IMMEDIATE 'GRANT role_caissier TO caissier01';
 
 -- ============================================================================
 -- 10) role_client
 -- ============================================================================
 
     FOR i IN 1 .. v_tables_client.COUNT LOOP
-            EXECUTE IMMEDIATE
-                'GRANT SELECT ON cine.' || v_tables_client(i) || ' TO role_client';
+            EXECUTE IMMEDIATE 'GRANT SELECT ON cine.' || v_tables_client(i) || ' TO role_client';
         END LOOP;
 
-        EXECUTE IMMEDIATE
-            'GRANT role_client TO client01';
+        EXECUTE IMMEDIATE 'GRANT role_client TO client01';
 END;
 /
